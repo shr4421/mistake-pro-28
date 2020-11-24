68838d2298dfd8d09d4a38d76ec169dbab1bda59
shr4421 <73051737+shr4421@users.noreply.github.com>
2020-11-24 17:09:10 +0530

    Add files via upload

---
+++ Tree.js
@@ -0,0 +1,29 @@
+class Tree {
+
+constructor(x,y,width,height){
+
+var option ={
+
+    isStatic:true
+
+}
+
+this.body=Bodies.rectangle(x,y,width,height,option);
+this.width = width;
+this.height = height;
+this.image=loadImage("Plucking mangoes/tree.png");
+World.add(world, this.body);
+
+}
+display (){
+
+var Treepos=this.body.position;
+
+push();
+translate(Treepos.x,Treepos.y);
+imageMode(CENTER);
+image(this.image,0,0,this.width,this.height);
+pop();
+
+}
+}
